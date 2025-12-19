7f79f4ea354dec03fa0d7a6f37ed4728133b6d5d
tbabineau <tbabine1@unb.ca>
2025-12-19 07:34:07 -0400

    Finished both parts of day 6

--- day6/part_one.py
+++ day6/part_one.py
@@ -1,4 +1,27 @@
 if __name__ == "__main__":
-    #fname = "input.txt"
-    fname = "test.txt"
+    fname = "input.txt"
+    #fname = "test.txt"
     file = open(fname)
+    grid = []
+    tot = 0
+    #Taking in the numbers as a big 2D array
+    for line in file:
+        row = []
+        line = line.strip().split(" ")
+        for var in line:
+            if(var != ''):
+                row.append(var)
+        grid.append(row)
+    cols = len(grid[0]) #num of columns
+    rows = len(grid) #num of rows
+    for i in range(cols):
+        op = grid[rows-1][i]
+        temp = (op == '*') #Will be a 1 if multiplying, 0 if adding
+        for j in range(rows-1):
+            if(op == '+'):
+                temp += int(grid[j][i])
+            else:
+                temp *= int(grid[j][i])
+        tot += temp
+    print(tot)
+    
--- day6/part_two.py
+++ day6/part_two.py
@@ -1,4 +1,37 @@
 if __name__ == "__main__":
-    #fname = "input.txt"
-    fname = "test.txt"
+    fname = "input.txt"
+    #fname = "test.txt"
     file = open(fname)
+    grid = []
+    tot = 0
+    temp = 0
+    #Taking in the numbers as a big 2D array
+    file = file.read().split("\n")
+    for line in file:
+        row = []
+        for var in line:
+            row.append(var)
+        grid.append(row)
+    cols = len(grid[0]) #num of columns
+    rows = len(grid) #num of rows
+    op = ''
+    for i in range(cols):
+        num = 0
+        for j in range(rows - 1):
+            if(grid[j][i] != ' '):
+                num = num*10 + int(grid[j][i])
+        if(grid[rows-1][i] != ' '):#new operation, have to change it
+            op = grid[rows-1][i]
+            tot += temp
+            if(op == '*'):
+                temp = 1
+            else:
+                temp = 0
+        if(num != 0):
+            if(op == '*'):
+                temp *= num
+            else:
+                temp += num
+    tot += temp
+            
+    print(tot)
