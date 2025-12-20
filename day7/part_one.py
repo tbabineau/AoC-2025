3413ad6a2692b7e2c0a5d85ee358a125312f4afb
tbabineau <tbabine1@unb.ca>
2025-12-20 17:41:49 -0400

    Finished day7 part one and two, part two using DP

--- day7/part_one.py
+++ day7/part_one.py
@@ -2,3 +2,28 @@ if __name__ == "__main__":
     #fname = "input.txt"
     fname = "test.txt"
     file = open(fname)
+    grid = []
+    split_count = 0
+    for line in file:
+        row = []
+        for char in line:
+            if(char != '\n'):
+                row.append(char)
+        grid.append(row)
+    
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows-1): #No splitting happens on the bottom row
+        for j in range(cols):
+            if(grid[i][j] == 'S'):
+                grid[i+1][j] = '|'
+            elif(grid[i][j] == '|'):
+                if(grid[i+1][j] == '^'):
+                    split_count += 1
+                    grid[i+1][j-1] = grid[i+1][j+1] = '|'
+                else:
+                    grid[i+1][j] = '|'
+    for line in grid:
+        print(line)
+    print(split_count)
--- day7/part_two.py
+++ day7/part_two.py
@@ -1,4 +1,42 @@
 if __name__ == "__main__":
-    #fname = "input.txt"
-    fname = "test.txt"
+    fname = "input.txt"
+    #fname = "test.txt"
     file = open(fname)
+    grid = []
+    for line in file:
+        row = []
+        for char in line:
+            if(char != '\n'):
+                row.append(char)
+        grid.append(row)
+    
+    rows = len(grid)
+    cols = len(grid[0])
+    num_grid = [[0 for _ in range(cols)] for _ in range(rows)]
+    for i in range(rows-1): #Filling out the grid, like in part one
+        for j in range(cols):
+            if(grid[i][j] == 'S'):
+                grid[i+1][j] = '|'
+                num_grid[i+1][j] = 1
+            elif(grid[i][j] == '|'):
+                if(grid[i+1][j] == '^'):
+                    grid[i+1][j-1] = grid[i+1][j+1] = '|'
+                    num_grid[i+1][j-1] = num_grid[i+1][j+1] = 1
+                else:
+                    grid[i+1][j] = '|'
+                    num_grid[i+1][j] = 1
+    
+    for i in range(rows-1, 0, -1):#Range goes from lower(inclusive) to upper (non-inclusive)
+        for j in range(cols):
+            if(grid[i][j] == '^'):
+                num_grid[i][j] = num_grid[i-1][j] = num_grid[i][j-1] + num_grid[i][j+1]
+                temp_row = i-1
+                while(grid[temp_row][j] == '|'):
+                    num_grid[temp_row][j] = num_grid[temp_row+1][j] #Send the change up the line
+                    temp_row -= 1
+                if(grid[temp_row][j] == 'S'):
+                    print(num_grid[temp_row+1][j])
+
+
+    
+    
